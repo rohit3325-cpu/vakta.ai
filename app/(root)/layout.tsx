56ce2fc13a2e6f4cf008ad6bb80e3888ab44bbc9
rohit <147339389+Rohit3325@users.noreply.github.com>
2026-03-08 03:53:44 +0530

    set up firebase,homepage and auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import  { ReactNode } from 'react'
 
 const Rootlayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>Home Page</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+           <Image src="/logo.svg" alt='Logo' width={38}
+           height={32} className="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10"/>
+           <h2 className="text-base sm:text-lg lg:text-xl font-semibold text-primary-100">
+             Vakta.AI
+           </h2>         
+        </Link>
+      </nav>
+
+      {children}
+    </div>
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,70 @@
+import Interviewcard from '@/components/Interviewcard'
+import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
     return (
-        <div>Home Page</div>
-    )
-}
+        <>
+       <section className="card-cta flex flex-col items-center gap-8 lg:flex-row lg:justify-between">
+
+             <div className="space-y-6 text-center lg:text-left">
+               <div className="space-y-6 mx-auto lg:mx-0">
+                 <h1 className="text-4xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
+                   Meet Your AI Coach
+                 </h1>
+           
+                 <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl lg:mx-0">
+                   Elevate your journey with AI-powered career coaching.
+                   Personalized. Precise. Progress-focused.
+                 </p>
+           
+                 <Button asChild className="btn-primary max-sm:w-full">
+                   <Link href="/interview">Start an Interview</Link>
+                 </Button>
+               </div>
+             </div>
+           
+             <Image
+               src="/robot.png"
+               alt="robo-dude"
+               width={400}
+               height={400}
+               className="w-[220px] sm:w-[300px] lg:w-[400px] h-auto"
+             />
+           
+           </section>   
+{/* interview section */}
+
+
+            {/* past interview */}
+           <section className='flex flex-col gap-6 mt-8'>
+            <h2>Your Past Interviews</h2>
+
+             <div className="interviews-section grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+                {dummyInterviews.map((interview)=>(
+                    <Interviewcard key={interview.id}{...interview} />
+                ))}
+             </div>
+            </section>  
+            
+
+
+            {/* next interview */}
+            <section className='flex flex-col gap-6 mt-8'>
+                  <h2>Pick Your Interview</h2>
+
+                  <div className="interviews-section grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+                      {dummyInterviews.map((interview)=>(
+                    <Interviewcard key={interview.id}{...interview} />
+                ))}
+                  </div>
+                </section> 
+
+
+                   </>
+               )
+           }
 export default Page
